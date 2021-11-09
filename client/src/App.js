bd7fb7bf25f9b2105a9080b57cd1706b33f7fe70
mateo <teodc4447@gmail.com>
2021-11-09 16:47:57 -0300

    routes working properly

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,20 @@ import './App.css';
 import Navbar from './components/navBar/navBar';
 import Home from './components/home/home';
 import Container from '@mui/material/Container';
-import Landing from './components/landing/landing';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Container fixed>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <Switch>
-          <Route path="/home">
-            <Navbar />
-            <Home />
-          </Route>
-        </Switch>
-      </Container>
-    </div>
+      <div className="App">
+        <Container fixed>
+          <Switch>
+            <Route path="/home">
+              <Navbar />
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+      </div>
     </BrowserRouter>
   );
 }
