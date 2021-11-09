03202984c5ea160f7d1c4600b204106a5f07b494
mateo <teodc4447@gmail.com>
2021-11-09 19:05:59 -0300

    landing structure

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 
-
 export default function Landing() {
   return (
     <div>
-        <h1>Landing</h1>
+      <h1>bienvenidos</h1>
+        <img src={"https://cdn.wallpapersafari.com/11/50/Yd4ibD.jpg"} alt="" height="700" width="1150"/>
         <Link to="/home">
         <Button>INICIO</Button>
         </Link>
