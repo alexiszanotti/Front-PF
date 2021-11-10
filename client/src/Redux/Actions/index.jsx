1c06fc32f4a4aa3d442d15cb93e8a3e7eaf77668
zanotti86 <alexiszanotti@gmail.com>
2021-11-10 01:43:15 +0100

    Finish allProducts Redux

--- client/src/Redux/Actions/index.jsx
+++ client/src/Redux/Actions/index.jsx
@@ -1,27 +1,32 @@
 import axios from "axios";
-import { GET_ALL_PRODUCTS, FILTER_PRODUCTS, SEARCH_PRODUCTS, DETAIL_PRODUCTS} from "./actionTypes";
+import { GET_ALL_PRODUCTS, FILTER_PRODUCTS, SEARCH_PRODUCTS, DETAIL_PRODUCTS } from "./actionTypes";
 
 export function getAllProducts() {
-  return {
-    type: GET_ALL_PRODUCTS,
+  return async function (dispatch) {
+    try {
+      let res = await axios("http://localhost:3001/products");
+
+      return dispatch({ type: GET_ALL_PRODUCTS, payload: res.data });
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
-export function filterProducts(){
+export function filterProducts() {
   return {
     type: FILTER_PRODUCTS,
-  }
+  };
 }
 
-export function searchProducts(){
+export function searchProducts() {
   return {
     type: SEARCH_PRODUCTS,
-  }
+  };
 }
 
-export function detailProducts(){
+export function detailProducts() {
   return {
     type: DETAIL_PRODUCTS,
-  }
+  };
 }
-
