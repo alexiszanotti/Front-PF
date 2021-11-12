2c4975cf243ddf663f7274d61828a5eab868c262
zanotti86 <alexiszanotti@gmail.com>
2021-11-12 12:57:38 +0100

    Form Create Product and validation form

--- client/src/Redux/Actions/index.jsx
+++ client/src/Redux/Actions/index.jsx
@@ -1,6 +1,19 @@
 import axios from "axios";
-import { GET_ALL_PRODUCTS, FILTER_PRICE, FILTER_DISCOUNT , FILTER_MODEL, FILTER_SEXO, SEARCH_PRODUCTS, DETAIL_PRODUCTS, SHOPPING_CART, REMOVE_CARD, FAVORITE, REMOVE_FAVORITE,POST_REG_USER } from "./actionTypes";
-
+import {
+  GET_ALL_PRODUCTS,
+  FILTER_PRICE,
+  FILTER_DISCOUNT,
+  FILTER_MODEL,
+  FILTER_SEXO,
+  SEARCH_PRODUCTS,
+  DETAIL_PRODUCTS,
+  SHOPPING_CART,
+  REMOVE_CARD,
+  FAVORITE,
+  REMOVE_FAVORITE,
+  POST_REG_USER,
+  GET_COLLECTIONS,
+} from "./actionTypes";
 
 export function getAllProducts() {
   return async function (dispatch) {
@@ -16,22 +29,24 @@ export function getAllProducts() {
 
 export function filterPrice(payload) {
   return {
-    type: FILTER_PRICE ,
-    payload
+    type: FILTER_PRICE,
+    payload,
   };
 }
 
-export function filterDiscount(payload){
-  return{
+export function filterDiscount(payload) {
+  return {
     type: FILTER_DISCOUNT,
-    payload
-  }
+    payload,
+  };
 }
 
-export function filterModel(collection){
+export function filterModel(collection) {
   return async function (dispatch) {
     try {
-      let res = await axios(`http://localhost:3001/categories/collection/?collection=${collection}`);
+      let res = await axios(
+        `http://localhost:3001/categories/collection/?collection=${collection}`
+      );
 
       return dispatch({ type: FILTER_MODEL, payload: res.data });
     } catch (error) {
@@ -40,7 +55,7 @@ export function filterModel(collection){
   };
 }
 
-export function filterSexo(gender){
+export function filterSexo(gender) {
   return async function (dispatch) {
     try {
       let res = await axios(`http://localhost:3001/categories/gender/?gender=${gender}`);
@@ -51,7 +66,6 @@ export function filterSexo(gender){
     }
   };
 }
-
 
 export function searchProducts(name) {
   try {
@@ -64,79 +78,94 @@ export function searchProducts(name) {
   }
 }
 
-export const detailProducts = (id) => {
-  try{
-    return async (dispatch) => {
+export const detailProducts = id => {
+  try {
+    return async dispatch => {
       let res = await axios(`http://localhost:3001/products/${id}`);
       return dispatch({
         type: DETAIL_PRODUCTS,
-        payload: res.data
-      })
-    }
-  }catch(error) {
-    console.error(error)
-
+        payload: res.data,
+      });
+    };
+  } catch (error) {
+    console.error(error);
   }
-}
+};
 
-export const shoppingCart = (id) => {
-  try{
-    return async (dispatch) => {
+export const shoppingCart = id => {
+  try {
+    return async dispatch => {
       let res = await axios(`http://localhost:3001/products/${id}`);
       return dispatch({
         type: SHOPPING_CART,
-        payload: res.data
-      })
-    }
-  }catch(error) {
-    console.error(error)
-
+        payload: res.data,
+      });
+    };
+  } catch (error) {
+    console.error(error);
   }
-}
+};
 
-export const removeCard = (id) => {
-  return{
+export const removeCard = id => {
+  return {
     type: REMOVE_CARD,
-    payload: id
-  }
-}
+    payload: id,
+  };
+};
 
-export const favorite = (id) => {
-  try{
-    return async (dispatch) => {
+export const favorite = id => {
+  try {
+    return async dispatch => {
       let res = await axios(`http://localhost:3001/products/${id}`);
       return dispatch({
         type: FAVORITE,
-        payload: res.data
-      })
-    }
-  }catch(error) {
-    console.error(error)
-
-  }
-}
-
-export const removeFavorite = (id) => {
-  return{
-    type: REMOVE_FAVORITE,
-    payload: id
-  }
-
-}
-
-export const postCreateUser = (payload) => {
-  
-  try {
-
-    return async (dispatch) => {
-
-      return await axios.post(`http://localhost:3001/createUser`,payload)
-
-    }
-    
+        payload: res.data,
+      });
+    };
   } catch (error) {
-
-    console.error(error)
-    
+    console.error(error);
   }
-}
+};
+
+export const removeFavorite = id => {
+  return {
+    type: REMOVE_FAVORITE,
+    payload: id,
+  };
+};
+
+export const postCreateUser = payload => {
+  try {
+    return async () => {
+      let res = await axios.post(`http://localhost:3001/createUser`, payload);
+      return res;
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createProduct = payload => {
+  try {
+    return async () => {
+      let res = await axios.post(`http://localhost:3001/createProduct`, payload);
+      return res;
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getCollection = payload => {
+  try {
+    return async dispatch => {
+      let res = await axios(`http://localhost:3001/categories`);
+      return dispatch({
+        type: GET_COLLECTIONS,
+        payload: res.data,
+      });
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
--- client/src/Redux/Reducer/index.jsx
+++ client/src/Redux/Reducer/index.jsx
@@ -1,4 +1,18 @@
-import { GET_ALL_PRODUCTS, FILTER_PRICE , SEARCH_PRODUCTS, DETAIL_PRODUCTS, FILTER_DISCOUNT, FILTER_MODEL,FILTER_SEXO, SHOPPING_CART, REMOVE_CARD, FAVORITE, REMOVE_FAVORITE } from "../Actions/actionTypes";
+import {
+  GET_ALL_PRODUCTS,
+  FILTER_PRICE,
+  SEARCH_PRODUCTS,
+  DETAIL_PRODUCTS,
+  FILTER_DISCOUNT,
+  FILTER_MODEL,
+  FILTER_SEXO,
+  SHOPPING_CART,
+  REMOVE_CARD,
+  FAVORITE,
+  REMOVE_FAVORITE,
+  CREATE_PRODUCT,
+  GET_COLLECTIONS,
+} from "../Actions/actionTypes";
 
 const initialState = {
   products: [],
@@ -6,6 +20,7 @@ const initialState = {
   detail: [],
   shoppingCart: [],
   favorite: [],
+  collections: [],
 };
 
 export default function rootReducer(state = initialState, action) {
@@ -16,68 +31,76 @@ export default function rootReducer(state = initialState, action) {
         products: action.payload,
         productsFilter: action.payload,
       };
-      case FILTER_PRICE :
-        let sortedArr = action.payload === "ASC"?
-        state.products.sort(function(a,b){
-          return a.salePrice - b.salePrice
-        }) :
-        state.products.sort(function(a,b){
-          return b.salePrice - a.salePrice
-        });
-        return{
-          ...state,
-          products: sortedArr,
-        }
-      case FILTER_DISCOUNT:
-        const products = state.productsFilter;
-        const statusFilter = action.payload === "All" ? products: products.filter(el => el.discount === action.payload)
-        return{
-          ...state,
-          products: statusFilter
-    
-        }
-      case FILTER_MODEL:
-        return {
-          ...state,
-          products: action.payload 
-        }
-      case FILTER_SEXO: 
-        return {
-          ...state,
-          products: action.payload 
-        }
-      case SEARCH_PRODUCTS:
-        return{
-          ...state,
-          products: action.payload,
-        }
-      case DETAIL_PRODUCTS:
-        return{
-          ...state,
-          detail: action.payload,
-        }
-      case SHOPPING_CART:
-        return{
-          ...state,
-          shoppingCart: state.shoppingCart.concat(action.payload),
-        }
-      case REMOVE_CARD:
-        return{
-          ...state,
-          shoppingCart: state.shoppingCart.filter(el => el.id !== action.payload),
-        }
-      case FAVORITE:
-        return{
-          ...state,
-          favorite: state.favorite.concat(action.payload),
-        }
-        case REMOVE_FAVORITE:
-        return{
-          ...state,
-          favorite: state.favorite.filter(el => el.id !== action.payload),
-
-        }
-
+    case FILTER_PRICE:
+      let sortedArr =
+        action.payload === "ASC"
+          ? state.products.sort(function (a, b) {
+              return a.salePrice - b.salePrice;
+            })
+          : state.products.sort(function (a, b) {
+              return b.salePrice - a.salePrice;
+            });
+      return {
+        ...state,
+        products: sortedArr,
+      };
+    case FILTER_DISCOUNT:
+      const products = state.productsFilter;
+      const statusFilter =
+        action.payload === "All" ? products : products.filter(el => el.discount === action.payload);
+      return {
+        ...state,
+        products: statusFilter,
+      };
+    case FILTER_MODEL:
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case FILTER_SEXO:
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case SEARCH_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case DETAIL_PRODUCTS:
+      return {
+        ...state,
+        detail: action.payload,
+      };
+    case SHOPPING_CART:
+      return {
+        ...state,
+        shoppingCart: state.shoppingCart.concat(action.payload),
+      };
+    case REMOVE_CARD:
+      return {
+        ...state,
+        shoppingCart: state.shoppingCart.filter(el => el.id !== action.payload),
+      };
+    case FAVORITE:
+      return {
+        ...state,
+        favorite: state.favorite.concat(action.payload),
+      };
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        favorite: state.favorite.filter(el => el.id !== action.payload),
+      };
+    case CREATE_PRODUCT:
+      return {
+        ...state,
+      };
+    case GET_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload,
+      };
     default:
       return state;
   }
