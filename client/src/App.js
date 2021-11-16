4ff3fe3cd96c44d489378e63b1d52d986e363798
mateo <teodc4447@gmail.com>
2021-11-16 16:51:18 -0300

    create panel control

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,11 @@ import ShopingCart from "./components/shopingCar/shopingCart";
 import Favorite from "./components/favorite/favorite";
 import createProduct from "./components/createProduct/createProduct";
 import GoShopping from "./components/goShopping/goShopping";
-import Admin from "./components/admin/homeA/homeA";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getAllProducts } from "./Redux/Actions/index";
 function App() {
   const dispatch = useDispatch();
-  const [admin, setAdmin] = React.useState(false);
   
   useEffect(() => {
     dispatch(getAllProducts());
@@ -25,26 +23,18 @@ function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        {
-          admin ? 
-          <div> 
-            <Route path='/admin' component={Admin} /> 
-            </div> : 
-          <div>
-            <Navbar />
-            <Route exact path='/' component={Landing} />
-            <Switch>
-              <Route path='/home' component={Home} />
-              <Route path='/login' component={Login} />
-              <Route path='/register' component={Register} />
-              <Route exact path='/detail/:id' component={Detail} />
-              <Route path='/carrito/:id' component={ShopingCart} />
-              <Route path='/favorites' component={Favorite} />
-              <Route path='/createProduct' component={createProduct} />
-              <Route path='/pago' component={GoShopping} />
-            </Switch>
-            </div>
-      }
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <Switch>
+          <Route path='/home' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route exact path='/detail/:id' component={Detail} />
+          <Route path='/carrito/:id' component={ShopingCart} />
+          <Route path='/favorites' component={Favorite} />
+          <Route path='/createProduct' component={createProduct} />
+          <Route path='/pago' component={GoShopping} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
