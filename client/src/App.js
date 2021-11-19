9876baeb806b9252bdc9e4f7c21d842b44a8ce8d
Emanuel Germano <emanuelgermano90@gmail.com>
2021-11-19 11:04:37 -0300

    logeo completo

--- client/src/App.js
+++ client/src/App.js
@@ -16,25 +16,54 @@ import EstadisticasA from "./components/admin/estadisticasA/estadisticasA";
 import AdminRoute from "./components/routes/adminRoute/adminRoute";
 import EditProduct from "./components/admin/editProduct/editProduct";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { getAllProducts } from "./Redux/Actions/index";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProducts, postCreateUser, getAllUsers, postUserLogin, userLogout } from "./Redux/Actions/index";
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+
+  const { user, isAuthenticated } = useAuth0()
+
   const dispatch = useDispatch();
   
   useEffect(() => {
     dispatch(getAllProducts());
+    dispatch(getAllUsers());
   }, [dispatch]);
 
+  const usersDB = useSelector( state => state.users)
+
+  const emailUsersDB = usersDB.map(e => e.email)
 
   const logIn = useSelector(state => state.userLogin);
-
-
-  console.log(logIn);
-
-
   
+  if(isAuthenticated) {
+ 
+    if((emailUsersDB.indexOf(user.email)) === -1) {
+      
+      dispatch(postCreateUser({
+        password: '1234passADIDAS',
+        email: user.email,
+        name: user.given_name ? user.given_name : 'name',
+        lastName: user.family_name ? user.family_name: 'lastName',
+        birthDay: "2000-01-01",
+        gender: "Other",
+      }));
+
+    } else {
+      
+      if(logIn.type === undefined) {
+
+        let email = user.email;
+
+        dispatch(postUserLogin({ email }))
+
+      }
+
+    }
+  
+  } 
+
   if (logIn.type === "Admin"){
     return(
       <BrowserRouter>
@@ -65,8 +94,6 @@ function App() {
           <Route path='/carrito/:id' component={ShopingCart} />
           <Route path='/favorites' component={Favorite} />
           <Route path='/pago' component={GoShopping} />
-
-          
 
         </Switch>
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,23 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import Store from "./Redux/Store";
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const { persistor, store } = Store
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 // persistor.purge()
 
 ReactDOM.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <App />
+        <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin} >
+          <App />
+        </Auth0Provider> 
       </PersistGate>
     </Provider>,
   document.getElementById("root")
