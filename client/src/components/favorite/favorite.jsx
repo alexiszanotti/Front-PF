c45348224836ab363440de20828ed10b35eb86f0
enzo388 <83272052+enzo388@users.noreply.github.com>
2021-11-21 21:57:45 -0300

    el teo no sabe nada XDXDXD na mentira ahi logre que se posteen los favoritos en la base de datos, faltaria hacer un condifcional para comprobar que si existen en la bae de datos te los muestre y esas cosas

--- client/src/components/favorite/favorite.jsx
+++ client/src/components/favorite/favorite.jsx
@@ -1,52 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from 'react-redux';
 import CardFavorite from "../cardFavorite/cardFavorite";
 import { Link } from "react-router-dom";
-import {emptyFavorites, addFavorites} from "../../Redux/Actions/index"
+import { emptyFavorites, postFavorite } from "../../Redux/Actions/index"
 import { useDispatch } from "react-redux";
 import "./favorite.css"
 
 export default function Favorite() {
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
     const favoritos = useSelector((state) => state.favorite);
     const userLogin = useSelector((state) => state.userLogin);
-    
+
 
     var hash = {};
-     let hola = favoritos.filter(function(current) {
-      var exists = !hash[current.id];
-      hash[current.id] = true;
-      return exists;
+    let hola = favoritos.filter(function (current) {
+        var exists = !hash[current.id];
+        hash[current.id] = true;
+        return exists;
     });
 
     const handleEmptyFavorites = () => {
         dispatch(emptyFavorites());
     }
 
-    let idUser = userLogin.id;
-    let idPorducs = favoritos.map((el) => el.id)
-    console.log(idPorducs)
+    const agregarfavorito = () => {
+        var idUser = userLogin.id
+        if(idUser) {
+            console.log(idUser)
+            let idPorducs = favoritos.map((el) => el.id)
+            if (idPorducs.length > 0) {
+                for(let i = 0 ; i < idPorducs.length ; i++) {
+                    dispatch(postFavorite({ userId: idUser, productId: idPorducs[i] }))
+                }
+                
+            }
+        }
+    }
+    useEffect(() => {
+        agregarfavorito()
+    }, [agregarfavorito]);
 
-    if (Object.keys(userLogin).length !== 0) {
-       if(Object.keys(favoritos).length !== 0){
-        dispatch(addFavorites(idPorducs))
-       }
-      }
+
+
+
+
     return (
         <div>
             <div className="favoriteContainer">
-            <h1>Mi lista de deseos</h1>
-            <h2>{hola.length} {hola.length === 1 ? "Artículo" : "Artículos"}       </h2>
-            <button onClick={handleEmptyFavorites}>BORRAR TODOS FAVORITOS</button>
+                <h1>Mi lista de deseos</h1>
+                <h2>{hola.length} {hola.length === 1 ? "Artículo" : "Artículos"}       </h2>
+                <button onClick={handleEmptyFavorites}>BORRAR TODOS FAVORITOS</button>
             </div>
             <br></br>
             <br></br>
             <div className="termo">
-            {
-                hola === undefined || hola.length === 0 ? <h1>...</h1> :
-                hola.map((products) => {
-                        return (
-                            
+                {
+                    hola === undefined || hola.length === 0 ? <h1>...</h1> :
+                        hola.map((products) => {
+                            return (
+
                                 <CardFavorite
                                     id={products.id}
                                     key={products.id}
@@ -55,12 +67,12 @@ export default function Favorite() {
                                     brand={products.collection.name}
                                     images={products.images[0]}
 
-                               />
-                                
-                                )
-                            })
-                        }
-                        </div>
+                                />
+
+                            )
+                        })
+                }
+            </div>
             <div>
                 <Link to="/home">
                     <button className="botonCart1">volver</button>
