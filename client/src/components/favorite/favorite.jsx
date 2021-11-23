065528b87871b53195682aee75071fabd5e93065
zanotti86 <alexiszanotti@gmail.com>
2021-11-23 13:02:29 +0100

    actualizar front

--- client/src/components/favorite/favorite.jsx
+++ client/src/components/favorite/favorite.jsx
@@ -1,86 +1,76 @@
 import React, { useEffect } from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import CardFavorite from "../cardFavorite/cardFavorite";
 import { Link } from "react-router-dom";
-import { emptyFavorites, postFavorite } from "../../Redux/Actions/index"
+import { emptyFavorites, postFavorite } from "../../Redux/Actions/index";
 import { useDispatch } from "react-redux";
-import "./favorite.css"
+import "./favorite.css";
 
 export default function Favorite() {
-    const dispatch = useDispatch();
-    const favoritos = useSelector((state) => state.favorite);
-    const userLogin = useSelector((state) => state.userLogin);
+  const dispatch = useDispatch();
+  const favoritos = useSelector(state => state.favorite);
+  const userLogin = useSelector(state => state.userLogin);
 
+  var hash = {};
+  let hola = favoritos.filter(function (current) {
+    var exists = !hash[current.id];
+    hash[current.id] = true;
+    return exists;
+  });
 
-    var hash = {};
-    let hola = favoritos.filter(function (current) {
-        var exists = !hash[current.id];
-        hash[current.id] = true;
-        return exists;
-    });
+  const handleEmptyFavorites = () => {
+    dispatch(emptyFavorites());
+  };
 
-    const handleEmptyFavorites = () => {
-        dispatch(emptyFavorites());
-    }
-
-    const agregarfavorito = () => {
-        var idUser = userLogin.id
-        if(idUser) {
-            console.log(idUser)
-            let idPorducs = favoritos.map((el) => el.id)
-            if (idPorducs.length > 0) {
-                for(let i = 0 ; i < idPorducs.length ; i++) {
-                    dispatch(postFavorite({ userId: idUser, productId: idPorducs[i] }))
-                }
-                
-            }
+  const agregarfavorito = () => {
+    var idUser = userLogin.id;
+    if (idUser) {
+      let idPorducs = favoritos.map(el => el.id);
+      if (idPorducs.length > 0) {
+        for (let i = 0; i < idPorducs.length; i++) {
+          dispatch(postFavorite({ userId: idUser, productId: idPorducs[i] }));
         }
+      }
     }
-    useEffect(() => {
-        agregarfavorito()
-    }, [agregarfavorito]);
+  };
+  useEffect(() => {
+    agregarfavorito();
+  }, [agregarfavorito]);
 
-
-
-
-
-    return (
-        <div>
-            <div className="favoriteContainer">
-                <h1>Mi lista de deseos</h1>
-                <h2>{hola.length} {hola.length === 1 ? "Artículo" : "Artículos"}       </h2>
-                <button onClick={handleEmptyFavorites}>BORRAR TODOS FAVORITOS</button>
-            </div>
-            <br></br>
-            <br></br>
-            <div className="termo">
-                {
-                    hola === undefined || hola.length === 0 ? <h1>...</h1> :
-                        hola.map((products) => {
-                            return (
-
-                                <CardFavorite
-                                    id={products.id}
-                                    key={products.id}
-                                    title={products.productName}
-                                    price={products.salePrice}
-                                    brand={products.collection.name}
-                                    images={products.images[0]}
-
-                                />
-
-                            )
-                        })
-                }
-            </div>
-            <div>
-                <Link to="/home">
-                    <button className="botonCart1">volver</button>
-                </Link>
-            </div>
-
-        </div>
-
-    )
+  return (
+    <div>
+      <div className='favoriteContainer'>
+        <h1>Mi lista de deseos</h1>
+        <h2>
+          {hola.length} {hola.length === 1 ? "Artículo" : "Artículos"}{" "}
+        </h2>
+        <button onClick={handleEmptyFavorites}>BORRAR TODOS FAVORITOS</button>
+      </div>
+      <br></br>
+      <br></br>
+      <div className='termo'>
+        {hola === undefined || hola.length === 0 ? (
+          <h1>...</h1>
+        ) : (
+          hola.map(products => {
+            return (
+              <CardFavorite
+                id={products.id}
+                key={products.id}
+                title={products.productName}
+                price={products.salePrice}
+                brand={products.collection.name}
+                images={products.images}
+              />
+            );
+          })
+        )}
+      </div>
+      <div>
+        <Link to='/home'>
+          <button className='botonCart1'>volver</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
-
--- client/src/components/goShopping/goShopping.jsx
+++ client/src/components/goShopping/goShopping.jsx
@@ -1,81 +1,73 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
-import Input from "@mui/material/Input";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import { useAuth0 } from '@auth0/auth0-react';
-import { Link, useHistory } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { modifyUser } from "../../Redux/Actions";
-import "./goShopping.css"
+import "./goShopping.css";
 
 export default function GoShopping() {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const usuario = useSelector((state) => state.userLogin);
-    const usr = useSelector((state) => state.users);
-    const [input, setInput] = useState({
-        id: usuario.id,
-    })
-    console.log(input, "teo puto")
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const usuario = useSelector(state => state.userLogin);
+  const usr = useSelector(state => state.users);
+  const [input, setInput] = useState({
+    id: usuario.id,
+  });
 
-    let usuarioLogeado = usr.filter((el) => el.id === usuario.id);
+  let usuarioLogeado = usr.filter(el => el.id === usuario.id);
 
+  const { loginWithRedirect } = useAuth0();
 
-    const { loginWithRedirect } = useAuth0();
+  const handleInputChange = function (e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleInputChange = function (e) {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value,
-        });
-    };
+  function handleSubmit(e) {
+    dispatch(modifyUser(input));
+    // alert("USUARIO RECARGADO");
+    setInput({
+      id: "",
+    });
+  }
 
-    function handleSubmit(e) {
-        dispatch(modifyUser(input));
-        // alert("USUARIO RECARGADO");
-        setInput({
-            id: "",
-        });
-      }
-
-    if (Object.keys(usuario).length === 0) {
-        loginWithRedirect();
-    } else {
-        return (
-            <div className="shoppingGeneral">
-                {/* <h1>Estas en el shoping</h1> */}
-                <form onSubmit={(e) => handleSubmit(e)} className="CreacionUsuario">
-                    <label>NOMBRE</label>
-                    <input type="text" name="name" onChange={handleInputChange} />
-                    <label>APELLIDO</label> 
-                    <input type="text" name="lastName" onChange={handleInputChange} />
-                    <label>EMAIL</label>
-                    <input type="text" name="email" onChange={handleInputChange} />
-                    <label>DOCUMENTO</label>
-                    <input type="text" name="document" onChange={handleInputChange} />
-                    <label>DIRECCION</label>
-                    <input type="text" name="address" onChange={handleInputChange} />
-                    <label>NUMERO DE CALLE</label>
-                    <input type="text" name="number" onChange={handleInputChange} />
-                    <label>PISO</label>
-                    <input type="text" name="floor" onChange={handleInputChange} />
-                    <label>DEPARTAMENTO</label>
-                    <input type="text" name="department" onChange={handleInputChange} />
-                    <label>LOCALIDAD</label>
-                    <input type="text" name="location" onChange={handleInputChange} />
-                    <label>PROVINCIA</label>
-                    <input type="text" name="province" onChange={handleInputChange} />
-                    <label>CODIGO POSTAL</label>
-                    <input type="text" name="cp" onChange={handleInputChange} />
-                    <label>TELEFONO</label>
-                    <input type="text" name="telephone" onChange={handleInputChange} />
-                    <button className="btn">Cargar datos</button>
-                </form>
-
-            </div>
-        );
-    }
+  if (Object.keys(usuario).length === 0) {
+    loginWithRedirect();
+  } else {
+    return (
+      <div className='shoppingGeneral'>
+        {/* <h1>Estas en el shoping</h1> */}
+        <form onSubmit={e => handleSubmit(e)} className='CreacionUsuario'>
+          <label>NOMBRE</label>
+          <input type='text' name='name' onChange={handleInputChange} />
+          <label>APELLIDO</label>
+          <input type='text' name='lastName' onChange={handleInputChange} />
+          <label>EMAIL</label>
+          <input type='text' name='email' onChange={handleInputChange} />
+          <label>DOCUMENTO</label>
+          <input type='text' name='document' onChange={handleInputChange} />
+          <label>DIRECCION</label>
+          <input type='text' name='address' onChange={handleInputChange} />
+          <label>NUMERO DE CALLE</label>
+          <input type='text' name='number' onChange={handleInputChange} />
+          <label>PISO</label>
+          <input type='text' name='floor' onChange={handleInputChange} />
+          <label>DEPARTAMENTO</label>
+          <input type='text' name='department' onChange={handleInputChange} />
+          <label>LOCALIDAD</label>
+          <input type='text' name='location' onChange={handleInputChange} />
+          <label>PROVINCIA</label>
+          <input type='text' name='province' onChange={handleInputChange} />
+          <label>CODIGO POSTAL</label>
+          <input type='text' name='cp' onChange={handleInputChange} />
+          <label>TELEFONO</label>
+          <input type='text' name='telephone' onChange={handleInputChange} />
+          <button className='btn'>Cargar datos</button>
+        </form>
+      </div>
+    );
+  }
 }
