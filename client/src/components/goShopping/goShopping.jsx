7fb25a813ee95c4e024cb2a5c73a95d037826d36
gastondigilio <gastondigilio@gmail.com>
2021-12-01 23:46:20 -0300

    formulario actualizado

--- client/src/components/goShopping/goShopping.jsx
+++ client/src/components/goShopping/goShopping.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { modifyUser, checkoutProducts } from "../../Redux/Actions";
+import { modifyUser, checkoutProducts, getAllUsers } from "../../Redux/Actions";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import { Link } from "react-router-dom";
 import "./goShopping.css";
-import swal from "sweetalert";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const validateForm = input => {
   let error = {};
-  if (!input.name) error.name = "El nombre del producto es requerido";
-  else if (!input.lastName) error.lastName = "El apellido es requerido";
-  else if (!input.email) error.email = "El mail es requerido";
-  else if (!input.document) error.document = "El documento es requerido";
-  else if (!input.address) error.address = "La dirección es requerida";
-  else if (!input.number) error.number = "La altura es requerida";
-  else if (!input.location) error.location = "La localidad es requerida";
-  else if (!input.cp) error.cp = "El código postal es requerido";
-  else if (!input.province) error.province = "La provincia es requerida";
-  else if (!input.telephone) error.telephone = "El teléfono postal es requerido";
+  if (!input.name) error.name = "Este campo es requerido";
+  else if (!input.lastName) error.lastName = "Este campo es requerido";
+  else if (!input.email) error.email = "Este campo es requerido";
+  else if (!input.document) error.document = "Este campo es requerido";
+  else if (!input.address) error.address = "Este campo es requerido";
+  else if (!input.number) error.number = "Este campo es requerido";
+  else if (!input.location) error.location = "Este campo es requerido";
+  else if (!input.cp) error.cp = "Este campo es requerido";
+  else if (!input.province) error.province = "Este campo es requerido";
+  else if (!input.telephone) error.telephone = "Este campo es requerido";
 
   return error;
 };
@@ -35,51 +36,90 @@ export default function GoShopping() {
   const dataBaseShopping = useSelector(state => state.ShoppingAlmacen);
   const [error, setError] = useState({});
 
+  let usuarioLogeado = usr.filter(el => el.id === usuario.id);
+  let name = usuarioLogeado.map((el) => el.name)
+  let lastName = usuarioLogeado.map((el) => el.lastName)
+  let email = usuarioLogeado.map((el) => el.email)
+  let document = usuarioLogeado.map((el) => el.document)
+  let address = usuarioLogeado.map((el) => el.address)
+  let number = usuarioLogeado.map((el) => el.number)
+  let location = usuarioLogeado.map((el) => el.location)
+  let cp = usuarioLogeado.map((el) => el.cp)
+  let floor = usuarioLogeado.map((el) => el.floor)
+  let department = usuarioLogeado.map((el) => el.department)
+  let province = usuarioLogeado.map((el) => el.province)
+  let telephone = usuarioLogeado.map((el) => el.telephone)
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     dispatch(getAllUsers());
+  //   },000)
+  // }, [dispatch]);
+
   const [input, setInput] = useState({
     id: usuario.id,
+    name: name.toString(),
+    lastName: lastName.toString(),
+    email: email.toString(),
+    document: document.toString(),
+    address: address.toString(),
+    number: number.toString(),
+    location: location.toString(),
+    cp: cp.toString(),
+    floor: floor.toString(),
+    department: department.toString(),
+    province: province.toString(),
+    telephone: telephone.toString(),
   });
 
-  let usuarioLogeado = usr.filter(el => el.id === usuario.id);
+  const errorSubmit = () => {
+    toast.error('Complete todos los campos requeridos', {
+      position: "bottom-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+    });
+  }
 
   const { loginWithRedirect } = useAuth0();
 
   const handleInputChange = e => {
-    e.preventDefault();
-
     setInput({ ...input, [e.target.name]: e.target.value });
     setError(validateForm({ ...input, [e.target.name]: e.target.value }));
   };
 
-  function handleSubmit(e) {
-    dispatch(modifyUser(input));
-    // alert("USUARIO RECARGADO");
-    if (Object.keys(error).length === 0) {
-      setInput({
-        id: "",
-      });
-    }
-  }
-
+  
   const handleCheckOut = () => {
     if (
-      usuarioLogeado.name === null ||
-      usuarioLogeado.lastName === null ||
-      usuarioLogeado.email === null ||
-      usuarioLogeado.documento === null ||
-      usuarioLogeado.address === null ||
-      usuarioLogeado.number === null ||
-      usuarioLogeado.location === null ||
-      usuarioLogeado.cp === null ||
-      usuarioLogeado.province === null ||
-      usuarioLogeado.telephone === null
-    ) {
-      swal("Error!", "Por favor, complete todos los campos requeridos!", "error");
-    } else {
+      usuarioLogeado[0].name === null ||
+      usuarioLogeado[0].lastName === null ||
+      usuarioLogeado[0].email === null ||
+      usuarioLogeado[0].document === null ||
+      usuarioLogeado[0].address === null ||
+      usuarioLogeado[0].number === null ||
+      usuarioLogeado[0].location === null ||
+      usuarioLogeado[0].cp === null ||
+      usuarioLogeado[0].province === null ||
+      usuarioLogeado[0].telephone === null
+      ) {
+        errorSubmit()
+      } else {
+        history.push("/checkout");
+      }
+    };
 
-      history.push("/checkout");
+    function handleSubmit(e) {
+      dispatch(modifyUser(input));
+      if (Object.keys(error).length === 0) {
+        setInput({
+          id: "",
+        });
+      }
     }
-  };
-
+    
   if (Object.keys(usuario).length === 0) {
     loginWithRedirect();
   } else {
@@ -95,8 +135,8 @@ export default function GoShopping() {
               <input
                 type='text'
                 name='name'
+                value={input.name}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.name)}
                 className='inputSarasa'
               />
               {error.name && <p className='error'>{error.name} </p>}
@@ -106,8 +146,8 @@ export default function GoShopping() {
               <input
                 type='text'
                 name='lastName'
+                value={input.lastName}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.lastName)}
               />
               {error.lastName && <p className='error'>{error.lastName} </p>}
             </div>
@@ -116,8 +156,8 @@ export default function GoShopping() {
               <input
                 type='text'
                 name='email'
+                value={input.email}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.email)}
               />
               {error.email && <p className='error'>{error.email} </p>}
             </div>
@@ -126,8 +166,8 @@ export default function GoShopping() {
               <input
                 type='number'
                 name='document'
+                value={input.document}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.document)}
               />
               {error.document && <p className='error'>{error.document} </p>}
             </div>
@@ -136,8 +176,8 @@ export default function GoShopping() {
               <input
                 type='text'
                 name='address'
+                value={input.address}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.address)}
               />
               {error.address && <p className='error'>{error.address} </p>}
             </div>
@@ -146,8 +186,8 @@ export default function GoShopping() {
               <input
                 type='number'
                 name='number'
+                value={input.number}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.number)}
               />
               {error.number && <p className='error'>{error.number} </p>}
             </div>
@@ -156,8 +196,8 @@ export default function GoShopping() {
               <input
                 type='text'
                 name='location'
+                value={input.location}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.location)}
               />
               {error.location && <p className='error'>{error.location} </p>}
             </div>
@@ -166,8 +206,8 @@ export default function GoShopping() {
               <input
                 type='number'
                 name='cp'
+                value={input.number}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.cp)}
               />
               {error.cp && <p className='error'>{error.cp} </p>}
             </div>
@@ -176,8 +216,8 @@ export default function GoShopping() {
               <input
                 type='number'
                 name='floor'
+                value={input.floor}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.floor)}
               />
             </div>
             <div className='sarasa'>
@@ -185,8 +225,8 @@ export default function GoShopping() {
               <input
                 type='text'
                 name='department'
+                value={input.department}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.department)}
               />
             </div>
             <div className='sarasa'>
@@ -194,8 +234,8 @@ export default function GoShopping() {
               <input
                 type='text'
                 name='province'
+                value={input.province}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.province)}
               />
               {error.province && <p className='error'>{error.province} </p>}
             </div>
@@ -204,8 +244,8 @@ export default function GoShopping() {
               <input
                 type='number'
                 name='telephone'
+                value={input.telephone}
                 onChange={handleInputChange}
-                placeholder={usuarioLogeado.map(el => el.telephone)}
               />
               {error.telephone && <p className='error'>{error.telephone} </p>}
             </div>
@@ -227,13 +267,12 @@ export default function GoShopping() {
             noValidate
             autoComplete='off'
           >
-            {/* <Link to='/checkout'> */}
             <Button onClick={handleCheckOut} variant='outlined' startIcon={<AttachMoneyIcon />}>
               Continuar al pago
             </Button>
-            {/* </Link> */}
           </Box>
         </div>
+        <ToastContainer />
       </div>
     );
   }
