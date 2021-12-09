52e3aaecdf52608d8ec1df1bc40f72c4ba5fd6db
zanotti86 <alexiszanotti@gmail.com>
2021-12-09 21:29:39 +0100

    About Us

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Pago from "./components/pago/pago";
 import DefaultError from "./components/error/error";
 import Checkuot from "./components/checkuot/checkuot";
 import MisCompras from "./components/misCompras/misCompras";
+import aboutUs from "./components/aboutUs/aboutUs";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -138,6 +139,7 @@ function App() {
             <Route path='/tarjeta' component={Pago} />
             <Route path='/checkout' component={Checkuot} />
             <Route path='/misCompras' component={MisCompras} />
+            <Route path='/aboutUs' component={aboutUs} />
             <Route path='*' component={DefaultError} />
           </Switch>
         </div>
